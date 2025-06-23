c4b140ff39d15c27edc5b207313ab2d14b837753
mbarushimanafabrice <123445030+MbarushimanaFabrice@users.noreply.github.com>
2025-06-23 10:40:22 +0300

    ff

--- code/basics/loops.py
+++ code/basics/loops.py
@@ -1,4 +1,23 @@
+
+# While loop example
+# This loop will print "Hello, World!" three times
 count =0
-while count<3:
+while count<3: 
+  # print(f"Hello, World!",count)
   count=count+1
-  print("Hello, World!")
+
+
+# For loop example
+# This loop will print numbers from 1 to 10
+
+for x in range(1,10,2):
+  pass
+  # print(x)
+  
+# For loop with a list
+# This loop will print each item in the lis
+
+my_list=[1,2,3,4,5,6,7,7,8,9,9,10]
+for x in my_list:
+  # print(x)
+  pass
